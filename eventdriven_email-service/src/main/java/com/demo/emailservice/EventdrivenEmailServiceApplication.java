086ab2dbaab0bc52c05e51f704c1eebd5723e7da
Anadi Mishra <62553697+anadi223@users.noreply.github.com>
2024-02-28 00:30:59 +0530

    Event Driven architecture based microservice project files related will be in eventdriven prefix

---
+++ eventdriven_email-service/src/main/java/com/demo/emailservice/EventdrivenEmailServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.emailservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventdrivenEmailServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventdrivenEmailServiceApplication.class, args);
+	}
+
+}
---
+++ eventdriven_stock-service/src/main/java/com/demo/stockservice/EventdrivenStockServiceApplication.java
@@ -0,0 +1,13 @@
+package com.demo.stockservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventdrivenStockServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventdrivenStockServiceApplication.class, args);
+	}
+
+}
